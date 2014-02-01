51e203d4310d8e54de92fefcb569c166d18b5ba6
Matias nnss Palomec <matiaspalomec@gmail.com>
2014-02-01 05:39:18 -0300

    added recaptcha and some fixes

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6Lftku0SAAAAANHlQ3S-RJJr_MbCk7jcra49D320'
+  config.private_key = '6Lftku0SAAAAAO4DtQGtwwgzEs9DyWoxYOuXBVJZ'
+  #domain = rdla.com.ar (global)
+  #config.proxy = 'http://myproxy.com.au:8080'
+end
